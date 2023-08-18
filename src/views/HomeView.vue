5eef4d1d47ba9bcaef9af3c50a31870fecb4e7fc
Chukwudi Ugorji <cp.ugorji@gmail.com>
2023-08-18 11:07:48 +0100

    done with other smaller services icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="Home" class="flex flex-col h-screen">
     <div class="md mb-auto">
+      <div class="my-20">
+        <img width="150" src="img/uber/uber2.svg" />
+      </div>
+
       <div class="grid grid-cols-2 gap-3 my-3">
         <ServiceSelectLarge text="Ride" imageWidth="74" image="ride" />
         <ServiceSelectLarge
@@ -10,6 +14,13 @@
           :promo="true"
         />
       </div>
+
+      <div class="grid grid-cols-4 gap-3 my-3">
+        <ServiceSelectSmall text="2 Wheels" image="2wheels" />
+        <ServiceSelectSmall text="Transit" image="transit" />
+        <ServiceSelectSmall text="Reserve" image="reserve" />
+        <ServiceSelectSmall text="Travel" image="travel" />
+      </div>
     </div>
   </div>
 </template>
@@ -17,4 +28,5 @@
 <script setup>
 // @ is an alias to /src
 import ServiceSelectLarge from "@/components/ServiceSelectLarge";
+import ServiceSelectSmall from "@/components/ServiceSelectSmall";
 </script>
