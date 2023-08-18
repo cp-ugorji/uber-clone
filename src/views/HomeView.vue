6fdcd47eef9b8004a5619b57ee0955aaecbff40c
Chukwudi Ugorji <cp.ugorji@gmail.com>
2023-08-18 12:16:45 +0100

    done with the major design for the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,12 +21,65 @@
         <ServiceSelectSmall text="Reserve" image="reserve" />
         <ServiceSelectSmall text="Travel" image="travel" />
       </div>
+
+      <div
+        @click="router.push('/directions')"
+        class="w-full bg-custom-color h-14 rounded-full flex items-center p-3"
+      >
+        <MagnifyIcon :size="40" />
+        <div class="ml-3 text-xs font-semibold text-gray-700">
+          Enter pick-up point
+        </div>
+      </div>
     </div>
+
+    <footer
+      class="w-full z-50 flex justify-around p-1 mb-2 border-t-2 border-t-gray-200"
+    >
+      <div class="grid place-items-center">
+        <HomeIcon :size="30" fillColor="#1f1e1e" />
+        <div
+          class="w-full -mt-1 text-xs text-center text-gray-800 font-semibold"
+        >
+          Home
+        </div>
+      </div>
+      <div @click="router.push('/directions')" class="grid place-items-center">
+        <MapMarkerIcon :size="30" fillColor="#b3b1b1" />
+        <div
+          class="w-full -mt-1 text-xs text-center text-gray-400 font-semibold"
+        >
+          Destination
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from "vue-router";
 // @ is an alias to /src
 import ServiceSelectLarge from "@/components/ServiceSelectLarge";
 import ServiceSelectSmall from "@/components/ServiceSelectSmall";
+import MagnifyIcon from "vue-material-design-icons/Magnify.vue";
+import HomeIcon from "vue-material-design-icons/Home.vue";
+import MapMarkerIcon from "vue-material-design-icons/MapMarker.vue";
+import { onMounted } from "@vue/runtime-core";
+import { useDirectionStore } from "@/store/direction-store";
+
+const router = useRouter();
+const direction = useDirectionStore();
+
+onMounted(() => {
+  direction.pickup = null;
+  direction.destination = null;
+});
 </script>
+
+<style lang="scss" scoped>
+#Home {
+  .bg-custom-color {
+    background-color: rgb(237, 237, 237);
+  }
+}
+</style>
